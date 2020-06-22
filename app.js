32f636698e9a011a8db5a0a2d15503dbdc719e64
thapaproj <66103404+thapaproj@users.noreply.github.com>
2020-06-22 15:34:24 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var app = express();
+var port = 8081;
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(port, () => {
+    console.log("Server listening on port " + port);
+});
